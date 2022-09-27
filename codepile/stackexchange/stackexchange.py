9054a9685260199485f563f6329f08bf4dd6a158
flowpoint <flowpoint>
2022-09-27 20:08:48 +0200

    add transcoding to intermediate arrow files

--- codepile/stackexchange/stackexchange.py
+++ codepile/stackexchange/stackexchange.py
@@ -14,6 +14,11 @@ from lm_dataformat import Archive
 import internetarchive as ia
 import py7zr
 
+import pyarrow as pa
+import pyarrow.parquet as pq
+import pyarrow.compute as pc
+import pyarrow.dataset as ds
+
 from codepile.dataset import DatasetInfo, DatasetSources, RawDataset, Scraper, Processor, Analyser, Dataset
 
 
@@ -71,6 +76,41 @@ def parse_xml(source_xml, output_dirpath) -> dict :
         while element.getprevious() is not None:
             del element.getparent()[0]
 
+# key, python type, pyarrow type
+post_types = (
+    ('Id', int, pa.uint32),
+    ('OwnerUserId', int, pa.int32),
+    ('ParentId', int, pa.uint32),
+    ('PostTypeId', int, pa.uint8),
+    ('Score', int, pa.int32), # can be negative
+    ('Title', str, pa.large_string),
+    ('Body', str, pa.large_string),
+    ('Tags', str, pa.large_string),
+    ('FavoriteCount', int, pa.uint32),
+    ('CreationDate', datetime.fromisoformat, pa.date64),
+    ('LastEditDate', datetime.fromisoformat, pa.date64),
+    )
+
+user_types = (
+    ('Id', int, pa.int32),
+    ('Reputation', int, pa.int32),
+    ('DisplayName', str, pa.large_string),
+    ('Views', int, pa.uint32),
+    ('UpVotes', int, pa.uint32),
+    ('DownVotes', int, pa.uint32),
+    ('AccountId', int, pa.int32),
+    ('ProfileImageUrl', str, pa.large_string),
+
+    ('CreationDate', datetime.fromisoformat, pa.date64),
+    ('LastAccessDate', datetime.fromisoformat, pa.date64),
+    )
+
+post_schema = pa.schema([(k,a()) for k,p,a in post_types])
+post_schema_python = {k:p for k,p,a in post_types}
+
+user_schema = pa.schema([(k,a()) for k,p,a in user_types])
+user_schema_python = {k:p for k,p,a in user_types}
+
 
 class StackExchangeCodeProcessor(Processor):
     def __init__(self, *args, **kwargs):
@@ -101,6 +141,17 @@ class StackExchangeCodeProcessor(Processor):
                 'Users.xml',
                 #'Votes.xml',
                 ]
+
+        self.target_schema_python = {
+                'Users.xml': user_schema_python,
+                'Posts.xml': post_schema_python
+                }
+
+        self.target_schema = {
+                'Users.xml': user_schema,
+                'Posts.xml': post_schema
+                #'Comments.xml',
+                }
 
     def save_to_jsonl(self, dict_):
         raise NotImplementedError
@@ -162,6 +213,10 @@ class StackExchangeCodeProcessor(Processor):
                     result.wait()
                     assert result.successful()
 
+                 # queue transcoding to intermediate format
+                for target in self.target_files:
+                    self.save_intermediate(domain, target)
+
 
     def extract_dumpfile(self, domain: str, input_uri: str):
         print(f'extracting {domain} {input_uri}')
@@ -192,6 +247,45 @@ class StackExchangeCodeProcessor(Processor):
             # all targets are in the same zip, so we unzip
             with py7zr.SevenZipFile(input_filepath, mode='r') as z:
                 z.extractall(path=output_dirpath)
+
+    def save_intermediate(self, domain: str, target: str):
+        output_dirpath = os.path.join(self.config.tmpdir, self.dataset_id, domain)
+        target_path = os.path.join(output_dirpath, target.replace('.xml', '.arrow'))
+        if os.path.isfile(target_path):
+            return
+
+        # todo
+        schema = self.target_schema[target]
+        schema_python = self.target_schema_python[target]
+
+        # note, this drops values that arent int the schema
+        def cast_dict_to_schema(schema, data: dict):
+            d = dict()
+            for k, type_ in schema.items():
+                if k in data:
+                    d[k] = type_(data[k])
+                else:
+                    d[k] = None
+            return d
+
+        with pa.OSFile(target_path, 'wb') as sink:
+            with pa.ipc.new_file(sink, schema) as writer:
+                xml_parse_stream = parse_xml(
+                        os.path.join(output_dirpath, target), 
+                        output_dirpath, 
+                        )
+
+                corrected_types_steam = map(
+                        partial(cast_dict_to_schema, schema_python),
+                        xml_parse_stream)
+
+                chunked_stream = chunked(corrected_types_steam, self.batch_size)
+                for chunk in tqdm(chunked_stream):
+                    batch = pa.RecordBatch.from_pylist(
+                            chunk,
+                            schema=schema)
+
+                    writer.write_batch(batch)
 
 
 class StackExchangeDataset(Dataset):
